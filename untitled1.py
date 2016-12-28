a2274d798d239c97731f12ad5ca8b8079ccf64d3
Vrushalii Desai <vrushd36@gmail.com>
2016-12-28 11:11:15 +0530

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 27 13:52:55 2016
+
+@author: Sonu
+"""
+
+import pandas as pd
+data = pd.DataFrame({'group': ['a', 'a', 'a', 'b','b', 'b', 'c', 'c','c'],
+                 'ounces': [4, 3, 12, 6, 7.5, 8, 3, 5, 6]})
+data
